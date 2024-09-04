645cf31ed9866bb5938c073cda2d8dd629737741
jin-park <jinparkdevelop@gmail.com>
2024-09-04 14:09:43 +0900

    update

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+async function connectToDevice() {
+  try {
+    console.log('Requesting Bluetooth Device...');
+    const device = await navigator.bluetooth.requestDevice({
+      // Filters can be used to narrow down the devices shown to the user.
+      acceptAllDevices: true,
+    });
+
+    console.log('Connecting to GATT Server...');
+    const server = await device.gatt.connect();
+
+    console.log('Connected to GATT Server:', server);
+
+    // Optionally, disconnect when done
+    // device.gatt.disconnect();
+  } catch (error) {
+    console.error('Error connecting to device:', error);
+  }
+}
